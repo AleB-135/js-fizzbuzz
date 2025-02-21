0313557f7137dc182651219ef54e2d290dc4660b
AleB-135 <alessiobrusca1999@outlook.com>
2025-02-21 18:01:00 +0100

    feat: Esercizio corretto

---
+++ js/js + html/script.js
@@ -0,0 +1,14 @@
+for (let i = 1; 1 <= 100; i++) {
+    let FizzBuzz
+    if (i % 3 === 0 && i % 5 === 0);
+        FizzBuzz = "FizzBuzz";
+}   else if (i % 3 === 0); {
+        FizzBuzz = "Fizz";
+}   else if ( i % 5 === 0); {
+        FizzBuzz = "Buzz";
+    else {
+        FizzBuzz = i;
+    }
+    console.log(FizzBuzz);
+}
+       

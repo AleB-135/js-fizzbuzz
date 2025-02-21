15607d1b3ae8fbb7fb48581fa83736ce6c4c637e
AleB-135 <alessiobrusca1999@outlook.com>
2025-02-21 18:01:39 +0100

    feat: Esercizio corretto

--- js/js + html/script.js
+++ js/js + html/script.js
@@ -1,13 +1,11 @@
 for (let i = 1; 1 <= 100; i++) {
-    let FizzBuzz
-    if (i % 3 === 0 && i % 5 === 0);
+    let FizzBuzz = i
+    if (i % 3 === 0 && i % 5 === 0); {
         FizzBuzz = "FizzBuzz";
-}   else if (i % 3 === 0); {
+    }   else if (i % 3 === 0); {
         FizzBuzz = "Fizz";
-}   else if ( i % 5 === 0); {
+    }   else if ( i % 5 === 0); {
         FizzBuzz = "Buzz";
-    else {
-        FizzBuzz = i;
     }
     console.log(FizzBuzz);
 }
